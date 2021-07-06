ce9c0db29b905bc679dc74aca7d780e3f27107c2
Renato Siqueira <renatoelysiqueira@gmail.com>
2021-07-06 14:08:57 -0300

    prepared to AWS Beanstalk

--- index.js
+++ index.js
@@ -4,51 +4,103 @@ const path = require('path')
 const multer = require('multer')
 const upload = multer({ dest: 'uploads' })
 const fs = require('fs')
-
 const s3 = require('s3')
-const paramsS3 = {
-  s3Options: {
-    accessKeyId: '',
-    secretAccessKey: '',
-    region: 'us-east-1'
-  }
+const port = process.env.port || 3000
+const s3Config = {
+  accessKeyId: process.env.S3_ACCESS_KEY || '',
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || '',
+  region: process.env.S3_REGION || 'us-east-1',
+  bucket: process.env.S3_BUCKET || 'fsm-devpleno'
 }
-const client = s3.createClient(paramsS3)
+
+const dbSettings = {
+  hostname: process.env.RDS_HOSTNAME || '127.0.0.1',
+  port: process.env.RDS_PORT || '3306',
+  dbName: process.env.RDS_DB_NAME || 'arquivos',
+  username: process.env.RDS_USERNAME || 'root',
+  password: process.env.RDS_PASSWORD || ''
+}
+
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize(dbSettings.dbName, dbSettings.username, dbSettings.password, {
+  dialect: 'mysql',
+  host: dbSettings.hostname
+})
+
+const Arquivo = sequelize.define('Arquivo', {
+  name: Sequelize.STRING
+})
+
+const client = s3.createClient({
+  s3Options: s3Config
+})
 
 const aws = require('aws-sdk')
-aws.config = new aws.Config({
-  accessKeyId: '',
-  secretAccessKey: '',
-  region: 'us-east-1'
-})
+aws.config = new aws.Config(s3Config)
 const s3SDK = new aws.S3()
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => res.render('index'))
-app.post('/upload', upload.single('foto'), (req, res) => {
-  const params = {
-    localFile: req.file.path,
-    s3Params: {
-      Bucket: 'fsm-devpleno',
-      Key: req.file.originalname,
-      ContentType: req.file.mimetype,
-      //ACL: 'public-read'
-    }
+app.get('/', async (req, res) => {
+  const arquivos = await Arquivo.findAll()
+  res.render('index', { arquivos })
+})
+app.get('/ver/:id', async (req, res) => {
+  const arquivo = await Arquivo.findById(req.params.id)
+  const s3File = {
+    Bucket: s3Config.bucket,
+    Key: arquivo.name,
+    Expires: 10
   }
-  const uploader = client.uploadFile(params)
-  uploader.on('end', () => {
-    fs.unlinkSync(req.file.path)
-    req.file.url = s3.getPublicUrl('fsm-devpleno', req.file.originalname, 'us-east-1')
-    const s3File = {
-      Bucket: 'fsm-devpleno',
-      Key: req.file.originalname,
-      Expires: 10
-    }
-    req.file.signedUrl = s3SDK.getSignedUrl('getObject', s3File)
-    res.send(req.file)
-  })
+  const signedUrl = s3SDK.getSignedUrl('getObject', s3File)
+  res.redirect(signedUrl)
 })
 
-app.listen(3000, () => console.log('running...'))
+const uploadToS3 = (file, key, mimetype, s3Config) => {
+  const params = {
+    localFile: file,
+    s3Params: {
+      Bucket: s3Config.bucket,
+      Key: key,
+      ContentType: mimetype,
+    }
+  }
+
+  return new Promise((resolve, reject) => {
+    try {
+      const uploader = client.uploadFile(params)
+      uploader.on('end', () => {
+        resolve()
+      })
+    } catch (error) {
+      console.log(error)
+    }
+
+  })
+}
+
+const removeFile = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.unlink(file, (err) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve()
+      }
+    })
+  })
+}
+
+app.post('/upload', upload.single('foto'), async (req, res) => {
+  await uploadToS3(req.file.path, req.file.originalname, req.file.mimetype, s3Config)
+  await removeFile(req.file.path)
+  const arquivo = await Arquivo.create({
+    name: req.file.originalname
+  })
+  res.redirect('/')
+})
+
+sequelize.sync().then(() => {
+  app.listen(port, () => console.log('running...'))
+})
